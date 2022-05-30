7cd58ce61a5b68dca80494678804519b2a9e0d79
Anafema-Forge1096 <98186504+Anafema-Forge1096@users.noreply.github.com>
2022-05-30 20:39:48 +0300

    Add files via upload

---
+++ django task/taskmanager/main/forms.py
@@ -0,0 +1,18 @@
+from dataclasses import fields
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "task"]
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите текст'
+        }),
+        "task": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите текст'
+        })
+        }
